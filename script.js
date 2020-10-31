f55a400454d64f67cafe17f3a9cd651606392e54
CofChips <qiwei.mod@gmail.com>
2020-10-31 08:16:47 -0700

    added logic to select and display characters

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let selectedChar = false;
+let selectedOpp = false;
 
 let Naruto = `
-<div class="col-lg-3 col-md-6">
+<div class="col-lg-3 col-md-6 m-auto">
                 <div class="card character" id="Naruto">
                     <div class="card-body">
                       <h5 class="card-title">Card title</h5>
@@ -15,12 +16,91 @@ let Naruto = `
 
 `
 
+let Itachi = `
+<div class="col-lg-3 col-md-6 m-auto">
+                <div class="card character" id="Itachi">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                      <img src="https://vignette.wikia.nocookie.net/naruto/images/b/bb/Itachi.png/revision/latest/scale-to-width-down/300?cb=20160125182202" alt="Itachi" style="width: 100%"/>
+                      
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                  </div>
+            </div>
+`
+
+let Rock = `
+<div class="col-lg-3 col-md-6 m-auto">
+                <div class="card character" id="Rock">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                      <img src="https://vignette.wikia.nocookie.net/naruto/images/9/97/Rock_Lee_Part_I.png/revision/latest/scale-to-width-down/300?cb=20181229065526" alt="Rock" style="width: 100%"/>
+                      
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                  </div>
+            </div>
+`
+
+let Sakura = `
+<div class="col-lg-3 col-md-6 m-auto">
+                <div class="card character" id="Sakura">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                      <img src="https://vignette.wikia.nocookie.net/naruto/images/6/64/Sakura_Part_1.png/revision/latest/scale-to-width-down/300?cb=20170726101444" alt="Sakura" style="width: 100%"/>
+                      <a href="#" class="card-link">Card link</a>
+                      <a href="#" class="card-link">Another link</a>
+                    </div>
+                  </div>
+            </div>
+`
+
 $(document).on("click", ".character", function () {
     console.log($(this).attr("id"))
-    if(selectedChar === false){
-        $("#selection").append(Naruto)
+    if (selectedChar === false) {
+        switch ($(this).attr("id")) {
+            case "Naruto":
+                $("#selection").append(Naruto)
+                break;
+            case "Itachi":
+                $("#selection").append(Itachi)
+                break;
+            case "Rock":
+                $("#selection").append(Rock)
+                break;
+            case "Sakura":
+                $("#selection").append(Sakura)
+                break;
+        }
+
+        $("#yourName").text($(this).attr("id"))
         selectedChar = true
-        $(this).attr("style","visibility: hidden")
+        $(this).attr("style", "visibility: hidden")
+    }
+    else if(selectedChar === true && selectedOpp === false) {
+        switch ($(this).attr("id")) {
+            case "Naruto":
+                $("#selectionOpp").append(Naruto)
+                break;
+            case "Itachi":
+                $("#selectionOpp").append(Itachi)
+                break;
+            case "Rock":
+                $("#selectionOpp").append(Rock)
+                break;
+            case "Sakura":
+                $("#selectionOpp").append(Sakura)
+                break;
+        }
+
+        $("#yourOpp").text($(this).attr("id"))
+        selectedOpp = true
+        $(this).attr("style", "visibility: hidden")
     }
 
 })
